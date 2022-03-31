4b28902b58eccd94659657f7f3555c3bebf45827
Taiwo Famurewa <maxteebabs@users.noreply.github.com>
2022-03-31 15:14:58 +0100

    string reversal

---
+++ string_reversal.py
@@ -0,0 +1,22 @@
+def reverseString(str: str):
+    splitted_str = list(str)
+    last_index = len(splitted_str) -1
+    stack = []
+    while(last_index >= 0):
+        stack.append(splitted_str[last_index])
+        last_index = last_index - 1
+    return "".join(stack)
+
+def reverseString1(str:str):
+    splitted_str = list(str)
+    i = 0
+    j = len(splitted_str) -1
+    while(i < j):
+        temp = splitted_str[i]
+        splitted_str[i] = splitted_str[j]
+        splitted_str[j] = temp
+        i = i + 1
+        j = j - 1
+    return "".join(splitted_str)
+result = reverseString1("my world")
+print(result)
