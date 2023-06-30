f8adb026ca0f9ecf3615fd6da08cd14ddedf2b4b
Taiwo Famurewa <maxteebabs@users.noreply.github.com>
2023-06-30 11:41:43 -0400

    Update mergesort.js

--- mergesort.js
+++ mergesort.js
@@ -22,3 +22,38 @@ function mergeSort(a){
 }
 const newArr = mergeSort(arr);
 console.log(newArr);
+
+// function mergeSort(arr) {
+//   // https://brilliant.org/wiki/heap-sort/
+//   // for worst case scenarios, use merge sort but has space complexity of 0(n)
+//   if(arr.length === 1) {
+//     return arr;
+//   }
+
+//   const mid = Math.floor(arr.length/2);
+//   const left = arr.slice(0, mid);
+//   const right = arr.slice(mid);
+  
+//   console.log('split::', left, right);
+//   return merge(
+//     mergeSort(left),
+//     mergeSort(right)
+//   )
+// }
+
+
+// function merge(arr1, arr2) {
+//   let leftIndex = 0;
+//   let rightIndex = 0;
+//   let result = [];
+//   while(leftIndex < arr1.length && rightIndex < arr2.length) {
+//     if(arr1[leftIndex] < arr2[rightIndex]) {
+//       result.push(arr1[leftIndex]);
+//       leftIndex++;
+//     }else {
+//       result.push(arr2[rightIndex]);
+//       rightIndex++;
+//     }
+//   }
+//   return result.concat(arr1.slice(leftIndex)).concat(arr2.slice(rightIndex));
+// }
